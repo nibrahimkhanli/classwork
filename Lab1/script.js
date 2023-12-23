c18699eda335c8342572cda2ab3fca76a80f5091
nibrahimkhanli <ibrahimkhanlinihad@gmail.com>
2023-12-23 19:53:09 +0400

    first commit

---
+++ Lab1/script.js
@@ -0,0 +1,54 @@
+////////////// TASK 1 //////////////
+
+// 1.Multiply 12 with 8, and console the result:
+let numa = 12
+let numb = 8
+let numc = numa*numb
+console.log(numc)
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+let number1 = 10
+let number2 = 2
+let f = number1/number2
+console.log(f)
+
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+let num1 = 20
+let num2 = 17
+console.log(num1+num2)
+
+
+/* 4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  then add them to varibale name (info) and console info. */
+
+  let name = 'Nihad', surname = 'Ibrahimkhanli', year = 1995
+  let info = name + ' ' + surname + ' ' + year
+  console.log(info)
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+
+console.log(17%12)
+
+// 6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+let cityName = 'Baku'
+cityName = 'Gence'
+console.log(cityName)
+
+////// TASK 2 //////////////
+
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+
+console.log(c);
+console.log(d);
+console.log(b);
+
+//////////// TASK 3 //////////////
+
+let e = 2;
+let x = 1 + (e *= 2);
+console.log(x);
+console.log(e)
